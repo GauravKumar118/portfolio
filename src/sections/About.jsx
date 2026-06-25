5edb56c9a638a28599c1acf2a49d8fd56f4c051c
Gaurav Saini <gauravsainiup12@gmail.com>
2026-06-25 15:22:26 +0530

    Fix hero button responsiveness

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -212,7 +212,7 @@ export default function About() {
             
             <Service />
             <Experience />
-            < Skill />
+            {/* < Skill /> */}
             <Footer />
 
 
